26ada8dc1bf54d3637cc9d351ac4f8410eb303f5
kempy <kempy@kempy.org>
2015-04-18 19:45:38 -0700

    Add x and y parameters and multiple gates

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,16 @@
 var init = function() {
   var stage = new createjs.Stage('mainCanvas');
-  var operator = createOperator();
-  var gate = createGate();
+  var operator = createOperator(50, 100);
+  var otherOperator = createOperator(50, 200);
+  var gate = createGate(150, 150);
+  var otherGate = createGate(150, 50);
   stage.addChild(gate);
+  stage.addChild(otherGate);
   stage.addChild(operator);
+  stage.addChild(otherOperator);
 
   // Register all gates on the stage.
-  stage.gates = [];
-  stage.gates[stage.gates.length] = gate;
+  stage.gates = [gate, otherGate];
   stage.update();
   function handleTick(event) {
     stage.update();
