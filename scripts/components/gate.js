554986503c29255757c5ab2124189b7aff825155
kempy <kempy@kempy.org>
2015-04-19 13:40:19 -0700

    Add connection lines for gates.

--- scripts/components/gate.js
+++ scripts/components/gate.js
@@ -11,9 +11,27 @@ Gate.prototype.containsOperator = function(operator) {
   return !!(this.operator && (this.operator.shape.id == operator.shape.id));
 };
 
+Gate.prototype.center = function() {
+  var bounds = this.shape.getTransformedBounds();
+  var xCenter = bounds.x + (bounds.width / 2);
+  var yCenter = bounds.y + (bounds.height / 2);
+  return {x: xCenter, y: yCenter};
+};
+
+Gate.prototype.drawTo = function(otherGate) {
+  var line = new createjs.Shape();
+  var gateCenter = this.center();
+  var otherGateCenter = otherGate.center();
+  line.graphics.beginStroke('Green').moveTo(
+  	gateCenter.x, gateCenter.y).lineTo(
+  	gateCenter.x, otherGateCenter.y).lineTo(
+  	otherGateCenter.x, otherGateCenter.y);
+  return line;
+};
+
 var createGateShape = function(x, y) {
   var snapBox = new createjs.Shape();
-  snapBox.graphics.beginStroke('Blue').drawRect(14, 14, 52, 52);
+  snapBox.graphics.beginStroke('Blue').beginFill('LightGreen').drawRect(14, 14, 52, 52);
   snapBox.x = x;
   snapBox.y = y;
   snapBox.setBounds(0, 0, 80, 80)
--- scripts/scenes/levels/demo.js
+++ scripts/scenes/levels/demo.js
@@ -2,12 +2,28 @@ var initDemoLevel = function(level) {
   var stage = level.stage;
   var operator = new Operator('AND', 50, 100);
   var otherOperator = new Operator('OR', 50, 200);
-  var gate = new Gate(150, 150);
-  var otherGate = new Gate(150, 50);
+  var gate = new Gate(150, 50);
+  var otherGate = new Gate(110, 150);
+  var line = gate.drawTo(otherGate);
+
+  // Add all lines, then all gates, then all operators.
+  stage.addChild(line);
+
   stage.addChild(gate.shape);
   stage.addChild(otherGate.shape);
+
   stage.addChild(operator.shape);
   stage.addChild(otherOperator.shape);
+  
   // Register all gates on the stage.
   stage.gates = [gate, otherGate];
+
+  var connection_points = ['a', 'b', 'c', 'gate1', 'gate2', 'output'];
+  var circuit = [
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 1]
+  ];
 };
