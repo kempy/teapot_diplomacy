e84aa8e656ceab7dd26bfec8969351e30d13e956
kempy <kempy@kempy.org>
2015-04-20 17:33:52 -0700

    Add level 1 (not gate)

--- scripts/main.js
+++ scripts/main.js
@@ -30,7 +30,7 @@ teapot.init = function() {
       "All of our inputs will be in binary: 1's and 0's. And we will use the following operators: NOT, AND, OR, NOR, NAND, XOR, XNOR.\n\n" +
       "By placing these operators in the correct gates on our circuit board, we'll create the desired output - and hopefully eventually world peace.\n\n" +
       "Click to play.");
-  instructionsScreen.params.nextLevel = 'demo';
+  instructionsScreen.params.nextLevel = 'level1';
 
   endScreen.params.text = (
       "Teapot Diplomacy\n\n" +
@@ -47,6 +47,8 @@ teapot.init = function() {
       "So, as you can see, this teapot has been successful in its mission! Stay tuned for our next project, where we make the perfect travel coffee maker for archaeologists on digs, in order to facilitate alertness and advance science.\n\n" +
       "Teapot Diplomacy! An unexpected weapon for good!");
   endScreen.params.nextLevel = 'start';
+
+  lm.createLevel('level1', createLevelOneFn('finish'));
 
   // Start at the demo level.
   lm.startLevel('start');
