f1396007db0bb911c6a1d876d1b696f4eab79f0e
Zoe Madden-Wood <maddenwood@gmail.com>
2015-04-20 18:08:51 -0700

    Adds reference article for operators.

--- scripts/main.js
+++ scripts/main.js
@@ -28,6 +28,7 @@ teapot.init = function() {
   instructionsScreen.params.text = (
       "Teapot Diplomacy Instructions\n\n" +
       "All of our inputs will be in binary: 1's and 0's. And we will use the following operators: NOT, AND, OR, NOR, NAND, XOR, XNOR.\n\n" +
+      "Please look at this page for reference on what they all do and keep it open! http://en.wikipedia.org/wiki/Logic_gate#Symbols\n\n" +
       "By placing these operators in the correct gates on our circuit board, we'll create the desired output - and hopefully eventually world peace.\n\n" +
       "Click to play.");
   instructionsScreen.params.nextLevel = 'level1';
