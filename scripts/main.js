0b0661ef9d4a49b437006ce8afc85cafaee7fb78
Zoe Madden-Wood <maddenwood@gmail.com>
2015-04-22 00:37:55 -0700

    Text changes on instructions.

--- scripts/main.js
+++ scripts/main.js
@@ -27,10 +27,10 @@ teapot.init = function() {
   startScreen.params.nextLevel = 'instructions';
   instructionsScreen.params.text = (
       "Teapot Diplomacy Instructions\n\n" +
-      "All of our inputs will be in binary: 1's and 0's. And we will use the following operators: NOT, AND, OR, NOR, NAND, XOR, XNOR.\n\n" +
-      "Please look at this page for reference on what they all do and keep it open! http://en.wikipedia.org/wiki/Logic_gate#Symbols\n\n" +
+      "All of our inputs will be in binary: 1's and 0's. And we will use the following operators: NOT, AND, OR, NOR, NAND, XOR, XNOR. All the operators will be clickable and you can place them into the gates.\n\n" +
+      "Please look at this page for reference on what the operators all do and keep it open! http://en.wikipedia.org/wiki/Logic_gate#Symbols\n\n" +
       "By placing these operators in the correct gates on our circuit board, we'll create the desired output - and hopefully eventually world peace.\n\n" +
-      "In the game, please click on all the 'sets' of input on the side, to try out the given input." +
+      "In the game, please click on all the 'sets' of input on the side, to try out the given input.\n\n" +
       "Click to play.");
   instructionsScreen.params.nextLevel = 'level1';
 
